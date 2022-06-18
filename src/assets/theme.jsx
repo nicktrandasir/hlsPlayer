fba946aac841d6e5bc0354d6e940baf4a4d8f1bd
n.trandasir <n.trandasir@dextechnology.com>
2022-06-18 10:05:24 +0300

    first commit

---
+++ src/assets/theme.jsx
@@ -0,0 +1,33 @@
+export const theme = {
+    rubberSize: {
+        desktop: "1199px",
+        tablet: "767px",
+        phone: "375px",
+    },
+    colors: {
+        red: "#CC122D",
+        redOpacity: "rgba(204, 18, 45, 0.1);",
+        redLightest: "#FFEEF1",
+        redDark: "#A2141B",
+        gold: "#C5B783",
+        blackRed: "#201118",
+        blueRed: "#2C222C",
+        black: "#0D1116",
+        blackLight: "#1B232C",
+        blackOpacity: "rgba(255, 255, 255, 0.2);",
+        grayDark: "#5C6168",
+        gray: "#A5ACB8",
+        grayLight: "#DCE1EA",
+        grayLightest: "#F9FAFB",
+        white: "#FFFFFF",
+        yellow: "#FFBD3E",
+        win1: "#FF6A13",
+        pink: "#F05A4F",
+        green: "#12CC54"
+    },
+    gradients: {
+        first: "linear-gradient(179.21deg, rgba(13, 17, 22, 0) 0.69%, #0d1116 99.33%)",
+        player: "linear-gradient(180deg, rgba(13, 17, 22, 0) 28.7%, #0D1116 100%)",
+        media: "linear-gradient(359.63deg, rgba(12, 18, 26, 0.8) 13.97%, rgba(14, 35, 86, 0.2) 69.27%, rgba(0, 0, 0, 0) 102.42%)"
+    },
+};
